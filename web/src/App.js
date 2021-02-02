3769df03d6943152c26876a4a6e426298897206d
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-02 22:06:16 +0100

    Add redux store

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import { applyMiddleware, createStore, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+
+import bookingsReducer from "./reducers/bookingsReducer";
+import loggedUserReducer from "./reducers/loggedUserReducer";
+import newUserFormReducer from "./reducers/newUserFormReducer";
+import usersReducer from "./reducers/usersReducer";
+
+const rootReducer = combineReducers({
+  bookings: bookingsReducer,
+  loggedUser: loggedUserReducer,
+  newUserForm: newUserFormReducer,
+  users: usersReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(reduxThunk))
+);
 
 function App() {
-  return <h1>hello bookly!</h1>;
+  return (
+    <Provider store={store}>
+      <h1>Hello Bookly!</h1>
+    </Provider>
+  );
 }
 
 export default App;
