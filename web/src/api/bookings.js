dcb7a71312baf833f565dc7ac4928027d9461f7f
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-02 23:16:01 +0100

    Add function to get all bookings

---
+++ web/src/api/bookings.js
@@ -0,0 +1,14 @@
+const API_URL = process.env.REACT_APP_API_URL;
+const SECURITY_TOKEN = process.env.REACT_APP_SECRET_TOKEN;
+
+export const getAll = (callback) => {
+  fetch(API_URL, {
+    method: "GET",
+    headers: {
+      "Security-Token": SECURITY_TOKEN,
+    },
+  })
+    .then((res) => res.json())
+    .then((json) => callback(json))
+    .catch((e) => alert(e));
+};
