c4fb72c5318eaffbbc225abdcbaedc96997aeb22
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-03 00:06:34 +0100

    Add basic router

--- web/src/App.js
+++ web/src/App.js
@@ -8,6 +8,17 @@ import loggedUserReducer from "./reducers/loggedUserReducer";
 import usersReducer from "./reducers/usersReducer";
 
 import LoginPage from "./pages/LoginPage";
+import SummaryPage from "./pages/SummaryPage";
+import AddUserPage from "./pages/AddUserPage";
+import AllUsersPage from "./pages/AllUsersPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
 const rootReducer = combineReducers({
   bookings: bookingsReducer,
@@ -23,7 +34,18 @@ const store = createStore(
 function App() {
   return (
     <Provider store={store}>
-      <LoginPage />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/summary" component={SummaryPage} />
+          <Route path="/addUser" component={AddUserPage} />
+          <Route path="/allUsers" component={AllUsersPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Router>
     </Provider>
   );
 }
