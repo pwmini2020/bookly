10c9a264dd589324d624aaa360fc0b0118b57e43
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-04 18:17:08 +0100

    Add button with fake credentials

--- web/src/pages/LoginPage.js
+++ web/src/pages/LoginPage.js
@@ -1,5 +1,18 @@
+import { authenticateCredentials } from "../api/loginApi";
+
 const LoginPage = (props) => {
-  return <h1>login page</h1>;
+  const fakeUsername = "testUsername";
+  const fakePassword = "testPass";
+  return (
+    <div>
+      <h1>login page</h1>
+      <button
+        onClick={() => authenticateCredentials(fakeUsername, fakePassword)}
+      >
+        Login with fake credentials
+      </button>
+    </div>
+  );
 };
 
 export default LoginPage;
