808c112b37e69ccc7988b9f120a2e358470e06ab
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-04 15:58:18 +0100

    Add links to navbar

--- web/src/components/Navbar.js
+++ web/src/components/Navbar.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
+import "../styles/Navbar.css";
+
 const Navbar = (props) => {
-  return <h2>navbar</h2>;
+  return (
+    <div className="Navbar">
+      <Link to="/summary">
+        <p>Summary</p>
+      </Link>
+
+      <Link to="/addUser">
+        <p>Add User</p>
+      </Link>
+
+      <Link to="/allUsers">
+        <p>Show All Users</p>
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
