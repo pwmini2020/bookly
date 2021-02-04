1596ee6bf49ce34d247a90e29cc59a1f92793c2f
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-04 22:33:56 +0100

    Use user token in get request

--- web/src/api/bookingsApi.js
+++ web/src/api/bookingsApi.js
@@ -1,14 +1,10 @@
 const API_URL = process.env.REACT_APP_API_URL;
-const SECURITY_TOKEN = process.env.REACT_APP_SECRET_TOKEN;
 
-export const getAll = (callback) => {
+export const getAll = (token) => {
   fetch(`${API_URL}/v1/bookings/`, {
     method: "GET",
     headers: {
-      "Security-Token": SECURITY_TOKEN,
+      "Security-Token": token,
     },
-  })
-    .then((res) => res.json())
-    .then((json) => callback(json))
-    .catch((e) => alert(e));
+  }).then((res) => res.json());
 };
