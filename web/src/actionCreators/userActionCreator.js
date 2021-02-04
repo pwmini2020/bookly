43675f088e347ca2d8c95e7c00a67208cf0dba30
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-04 23:42:38 +0100

    Change comment

--- web/src/actionCreators/userActionCreator.js
+++ web/src/actionCreators/userActionCreator.js
@@ -28,7 +28,7 @@ export const loginUser = (login, password) => {
       const response = await authenticateCredentials(login, password);
       dispatch(saveToken(response.token));
       dispatch(loginSucceeded());
-      // route to summary
+      // redirect to summary
     } catch (error) {
       console.error(error);
       dispatch(loginFailed());
