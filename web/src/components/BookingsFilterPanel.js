2faa0dea57a773e576bab2166c196d305ab76b1b
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-05 16:00:34 +0100

    Add empty filter panel component

---
+++ web/src/components/BookingsFilterPanel.js
@@ -0,0 +1,9 @@
+const BookingsFilterPanel = (props) => {
+  return (
+    <div>
+      <h1>filter panel</h1>
+    </div>
+  );
+};
+
+export default BookingsFilterPanel;
--- web/src/pages/SummaryPage.js
+++ web/src/pages/SummaryPage.js
@@ -1,7 +1,10 @@
+import BookingsFilterPanel from "../components/BookingsFilterPanel";
+
 const SummaryPage = (props) => {
   return (
     <div>
       <h1>summary page</h1>
+      <BookingsFilterPanel />
     </div>
   );
 };
