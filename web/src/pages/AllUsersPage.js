6daeaa2aada5833243684fa88054186d86b2ab9e
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-05 14:02:47 +0100

    Remove unused import

--- web/src/pages/AllUsersPage.js
+++ web/src/pages/AllUsersPage.js
@@ -1,24 +1,36 @@
-
-import Navbar from "../components/Navbar";
-import UserData from "../PlaceholderData/AllUsers.json"
-import UserListItem from "../components/UserListItem"
-import NavButtons from "../components/NavButtons"
+import UserData from "../PlaceholderData/AllUsers.json";
+import UserListItem from "../components/UserListItem";
+import NavButtons from "../components/NavButtons";
 
 const AllUsersPage = (props) => {
   return (
     <div>
       <h1>all users page</h1>
       <div className="UserList">
-        <div className="UserListItem" style={{background: "lightgray", height: "30px"}}>
-            <div className="UserListItemColumn" style={{flex: 0.4}}>
-                <text style={{fontWeight: "bold"}}>Username</text>
-            </div>
-            <div className="UserListItemColumn" style={{flex: 0.6}}>
-                <text style={{fontWeight: "bold"}}>Last Active</text>
-            </div>
+        <div
+          className="UserListItem"
+          style={{ background: "lightgray", height: "30px" }}
+        >
+          <div className="UserListItemColumn" style={{ flex: 0.4 }}>
+            <text style={{ fontWeight: "bold" }}>Username</text>
+          </div>
+          <div className="UserListItemColumn" style={{ flex: 0.6 }}>
+            <text style={{ fontWeight: "bold" }}>Last Active</text>
+          </div>
         </div>
-        {UserData.map((user) => <UserListItem t1 = {user.username} t2={new Date(user.lastActive).toDateString()}/>)}
-        <NavButtons pageNumber={1} pageTotal={10} nextPage={() => {console.log("Next button pressed")}} />
+        {UserData.map((user) => (
+          <UserListItem
+            t1={user.username}
+            t2={new Date(user.lastActive).toDateString()}
+          />
+        ))}
+        <NavButtons
+          pageNumber={1}
+          pageTotal={10}
+          nextPage={() => {
+            console.log("Next button pressed");
+          }}
+        />
       </div>
     </div>
   );
