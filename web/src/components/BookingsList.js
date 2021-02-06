d4d32af6c000b6c1e8ac8b9cfb168366da3dd431
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-06 18:29:45 +0100

    Add navbuttons

--- web/src/components/BookingsList.js
+++ web/src/components/BookingsList.js
@@ -1,5 +1,27 @@
+import bookingsData from "../mockData/allBookings.json";
+
+import BookingsListItem from "./BookingsListItem";
+import NavButtons from "./NavButtons";
+
 const BookingsList = (props) => {
-  return <p>list</p>;
+  return (
+    <div>
+      {bookingsData.map((booking) => (
+        <BookingsListItem key={booking.id} />
+      ))}
+
+      <NavButtons
+        pageNumber={1}
+        pageTotal={10}
+        previousPage={() => {
+          console.log("Previous button pressed.");
+        }}
+        nextPage={() => {
+          console.log("Next button pressed");
+        }}
+      />
+    </div>
+  );
 };
 
 export default BookingsList;
