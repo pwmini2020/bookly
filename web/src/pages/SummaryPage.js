9baeb4e73d1ddbc27eae050bb583e797220326b9
JacekCzupyt <kuba.czupyt@gmail.com>
2021-02-06 20:55:04 +0100

    First draft of the DetailsDisplay component

--- web/src/pages/SummaryPage.js
+++ web/src/pages/SummaryPage.js
@@ -1,9 +1,13 @@
 import BookingsFilterPanel from "../components/BookingsFilterPanel";
 
+import MockBookings from "../mockData/AllBookings.json";
+import DetailsDisplay from "../components/DetailsDisplay";
+
 const SummaryPage = (props) => {
   return (
     <div>
       <h1>summary page</h1>
+      <DetailsDisplay data={MockBookings[2]} />
       <BookingsFilterPanel />
     </div>
   );
