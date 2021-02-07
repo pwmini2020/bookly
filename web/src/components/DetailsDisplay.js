cab892856f75560009b82f4c50d908d3ad883288
JacekCzupyt <kuba.czupyt@gmail.com>
2021-02-07 01:13:22 +0100

    Reworked DetailsDisplay using bootstrap

--- web/src/components/DetailsDisplay.js
+++ web/src/components/DetailsDisplay.js
@@ -1,50 +1,82 @@
 import "../styles/DetailsDisplay.css";
 
+import Card from "react-bootstrap/Card";
+
 const DetailsDisplay = (props) => {
   const DisplayDetails = () => {
     if (props.data.bookingType === "Car") {
       return (
         <div>
-          <p>Price: {props.data.Details.dailyPrice} PLN per day</p>
-          <p>Model: {props.data.Details.model}</p>
-          <p>Location: {props.data.Details.location}</p>
-          <p>Plate number: {props.data.Details.plateNumber}</p>
-          <p>Description: {props.data.Details.description}</p>
+          Price: {props.data.Details.dailyPrice} PLN per day
+          <br />
+          Model: {props.data.Details.model}
+          <br />
+          Location: {props.data.Details.location}
+          <br />
+          Plate number: {props.data.Details.plateNumber}
+          <br />
+          Description: {props.data.Details.description}
+          <br />
         </div>
       );
     }
     if (props.data.bookingType === "Parking") {
       return (
         <div>
-          <p>Price: {props.data.Details.dailyPrice} PLN per day</p>
-          <p>Location: {props.data.Details.location}</p>
-          <p>Space number: {props.data.Details.SpaceNo}</p>
-          <p>Description: {props.data.Details.description}</p>
+          Price: {props.data.Details.dailyPrice} PLN per day
+          <br />
+          Location: {props.data.Details.location}
+          <br />
+          Space number: {props.data.Details.SpaceNo}
+          <br />
+          Description: {props.data.Details.description}
+          <br />
         </div>
       );
     }
     if (props.data.bookingType === "Flat") {
       return (
         <div>
-          <p>Price: {props.data.Details.dailyPrice} PLN per day</p>
-          <p>Location: {props.data.Details.location}</p>
-          <p>Number of rooms: {props.data.Details.Number_of_rooms}</p>
-          <p>Description: {props.data.Details.description}</p>
+          Price: {props.data.Details.dailyPrice} PLN per day
+          <br />
+          Location: {props.data.Details.location}
+          <br />
+          Number of rooms: {props.data.Details.Number_of_rooms}
+          <br />
+          Description: {props.data.Details.description}
+          <br />
         </div>
       );
     }
   };
 
-  return (
-    <div className="DetailsDisplay">
+  /*return (
+    <div className="jumbotron p-3">
       <div style={{ background: "lightgray", textAlign: "center" }}>
-        <p>Details for booking {props.data.id}</p>
+        Details for booking {props.data.id}<br/>
       </div>
-      <p>Booking type: {props.data.bookingType}</p>
-      <p>Date Begun: {props.data.startDate}</p>
-      <p>Date Ended: {props.data.endDate}</p>
+      Booking type: {props.data.bookingType}<br/>
+      Date Begun: {props.data.startDate}<br/>
+      Date Ended: {props.data.endDate}<br/>
       {DisplayDetails()}
     </div>
+  );*/
+
+  return (
+    <Card variant="info" className="DetailsDisplay">
+      <Card.Body>
+        <Card.Title>Details for booking {props.data.id}</Card.Title>
+        <Card.Text>
+          Booking type: {props.data.bookingType}
+          <br />
+          Date Begun: {new Date(props.data.startDate).toDateString()}
+          <br />
+          Date Ended: {new Date(props.data.endDate).toDateString()}
+          <br />
+          {DisplayDetails()}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
