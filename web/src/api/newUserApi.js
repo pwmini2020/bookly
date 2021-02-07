0ce8bc26243ce30bd27eb5869f3170d1c8884845
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-07 16:27:28 +0100

    Add posting new user

--- web/src/api/newUserApi.js
+++ web/src/api/newUserApi.js
@@ -1,2 +1,11 @@
-// NOT FINISHED
-export const postNewUser = (newUserData) => {};
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const postNewUser = (newUserData) => {
+  return fetch(`${API_URL}/v1/users/`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json;charset=UTF-8",
+    },
+    body: JSON.stringify(newUserData),
+  }).then((res) => res.json());
+};
