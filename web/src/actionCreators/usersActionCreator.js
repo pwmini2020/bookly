938912f559e47221b67d1b679226b36d4c165898
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-07 22:15:57 +0100

    Add actions for pages

--- web/src/actionCreators/usersActionCreator.js
+++ web/src/actionCreators/usersActionCreator.js
@@ -29,12 +29,34 @@ const saveUsers = (users) => {
   };
 };
 
-export const fetchUsers = (token) => {
+export const nextPageUsers = () => {
+  return {
+    type: "NEXT_PAGE_USERS",
+  };
+};
+
+export const previousPageUsers = () => {
+  return {
+    type: "PREVIOUS_PAGE_USERS",
+  };
+};
+
+const saveTotalPagesUsers = (totalPages) => {
+  return {
+    type: "SAVE_TOTAL_PAGES_USERS",
+    payload: {
+      totalPages: totalPages,
+    },
+  };
+};
+
+export const fetchUsers = (token, paginationSettings) => {
   return async (dispatch) => {
     try {
       dispatch(fetchUsersInProgress());
-      const responseBody = await getUsers(token);
-      dispatch(saveUsers(responseBody));
+      const responseBody = await getUsers(token, paginationSettings);
+      dispatch(saveUsers(responseBody.content));
+      dispatch(saveTotalPagesUsers(responseBody.totalPages));
       dispatch(fetchUsersSucceeded());
     } catch (error) {
       console.error(error);
