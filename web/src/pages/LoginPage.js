785cc8658c2f9c7030aacfc128cd3be23b5704e3
madpeh <48161600+madpeh@users.noreply.github.com>
2021-02-08 12:08:36 +0100

    Change default credentials

--- web/src/pages/LoginPage.js
+++ web/src/pages/LoginPage.js
@@ -13,7 +13,7 @@ const mapStateToProps = (state) => ({
 });
 
 const LoginPage = (props) => {
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState("ugabuga");
   const [password, setPassword] = useState("");
 
   // after a user logs in switch the page to /summary
